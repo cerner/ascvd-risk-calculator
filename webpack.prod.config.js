5eefa4a173e381decb693eff8116f51928bfd166
sharmander <jason.shar@cerner.com>
2019-12-04 13:59:19 -0600

    no more webpack merge

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,12 +1,12 @@
 const config = require('./webpack.dev.config.js');
 const intlConfig = require('./webpack.intl.config.js');
 const path = require('path');
-const webpackMerge = require('webpack-merge');
 
-module.exports = webpackMerge(intlConfig, config, {
-  mode: 'production',
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-});
+config.output = {
+  path: path.join(__dirname, 'build'),
+  filename: 'bundle.js',
+};
+
+config.mode = 'production';
+
+module.exports = [intlConfig, config];
